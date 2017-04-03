44b413c48d9486056a0661476551f1c21ca2659b
Bruno Lucas <bruno-lucas@msn.com>
2017-04-03 19:00:06 -0300

    Desenha o player no canvas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var cnv = document.querySelector('canvas'),
-WIDTH = cnv.width,
-HEIGHT = cnv.height,
+WIDTH = cnv.width = 500,
+HEIGHT = cnv.height = 500,
 ctx = cnv.getContext('2d');
 
 function Player(x, y, width, height, color){
@@ -9,14 +9,19 @@ function Player(x, y, width, height, color){
     this.width = width;
     this.height = height;
     this.color = color;
+    this.speed = 5;
 }
+
+var player = new Player(50, 50, 50, 50, '#f00');
 
 function move(){
 
 }
 
 function render(){
-
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    ctx.fillStyle = player.color;
+    ctx.fillRect(player.x, player.y, player.width, player.height);
 }
 
 function update(){
