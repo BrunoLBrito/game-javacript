a7217f4cf7ad74dd9456cf13c8e4cc4271aadb01
Bruno Lucas <bruno-lucas@msn.com>
2017-04-03 17:53:02 -0300

    criando objeto do player

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ WIDTH = cnv.width,
 HEIGHT = cnv.height,
 ctx = cnv.getContext('2d');
 
+function Player(x, y, width, height, color){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+}
+
 function move(){
 
 }
